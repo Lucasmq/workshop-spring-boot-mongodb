8d24f5fac48df4d7d7cd0de9f8c62b1cc459d201
Lucas Queiroz <lucasmedeirosq@gmail.com>
2019-03-18 17:15:24 -0300

    Obtendo post por ID

---
+++ src/main/java/com/lucas/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.lucas.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucas.workshopmongo.domain.Post;
+import com.lucas.workshopmongo.repository.PostRepository;
+import com.lucas.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired // injeção de dependencia automatica do spring
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); // caso o obj seja null , retorna a msg
+	}
+	
+}
